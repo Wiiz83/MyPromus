0703036e24261295e34ea201f6074b56f197df09
ferran294 <ferran294@gmail.com>
2015-04-20 16:09:19 +0200

    Corrected some bugs of dashboard and added the dateCreation of the user

--- controller/dashboard.php
+++ controller/dashboard.php
@@ -4,7 +4,7 @@ Controller of the dashboard page */
 
 include $_SERVER['DOCUMENT_ROOT'].'/myPromus/includes/session_checker.inc.php'; //Check if the session has expired,in that case redirect to the main page
 require $_SERVER['DOCUMENT_ROOT'].'/myPromus/model/dashboard_model.php';
-require $_SERVER['DOCUMENT_ROOT'].'/myPromus/classes/event.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/myPromus/classes/event.class.php';
 
 //Starts session and gets username
 session_start();
--- model/dashboard_model.php
+++ model/dashboard_model.php
@@ -9,24 +9,29 @@ require $_SERVER['DOCUMENT_ROOT'].'/myPromus/classes/event.class.php';
 
 
 /*Get the  nearest events to show in the main dashboard, the number of events that we want to show
-will be passed as an argument in the method
+will be passed as an argument in the method,the function returns an array of Events
 */
 
 function getNearestEvents($username,$numberOfEvents){		//The id is the username of the user that is asking for the events
 	global $link;
-	$sql="SELECT * FROM Event WHERE username='$username' ORDER BY date";
-	$result=mysqli_query($link,$sql);
+	$todayDate=date("Y-m-d");
+	
+	$sql="SELECT * FROM event WHERE user_id='$username' ORDER BY date";
+	$result=mysqli_query($link,$sql) or die(mysqli_error($link));
 	$rowsNumber=mysqli_num_rows($result);
 	
 	if($rowsNumber<$numberOfEvents){
 		$numberOfEvents=$rowsNumber;	//If in the database there are less events than asked, the number of
 	}									//events showed will change
-
+	
 	for($i=0;$i<$numberOfEvents;$i++){
 		$eventInfo=mysqli_fetch_assoc($result);
 		$events[]=new Event($eventInfo['id'],$eventInfo['user_id'],$eventInfo['name'],$eventInfo['date'],$eventInfo['place'],$eventInfo['description']);
+		
 	}
 
+	print_r("$events");
+	
 	if(isset($events)){
 		return $events;
 	}else{
