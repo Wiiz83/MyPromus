5c02f38fdc80547460856f3df0f4221b5a11b06c
ferran294 <ferran294@gmail.com>
2015-04-22 18:17:52 +0200

    Added friend model in the controller

--- controller/friends.php
+++ controller/friends.php
@@ -3,9 +3,10 @@
 Controller of the friends page */
 
 include $_SERVER['DOCUMENT_ROOT'].'/myPromus/includes/session_checker.inc.php'; //Check if the session has expired,in that case redirect to the main page
+include $_SERVER['DOCUMENT_ROOT'].'/myPromus/model/friends_model.php';
 
-
-
-
+session_start();
+$userId=$_SESSION['userId'];
+$friends=getFriends($userId);
 
 ?>
