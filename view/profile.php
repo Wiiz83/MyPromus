c11847d2be43563f100e5f80b463fb035d7e2103
Wiiz83 <uzanlah@gmail.com>
2015-04-23 19:24:11 +0100

    caca

---
+++ view/profile.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+
+        <head>
+                <meta charset="utf-8">
+                <title>My Promus :: Home Page</title>
+                <link rel="stylesheet" href="assets/stylesheets/styles.css">
+                <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700,500,700italic,500italic,400italic,300italic,900,900italic' rel='stylesheet' type='text/css'> 
+        </head>
+
+        <body>
+                <div class="top-menu">
+                        <div class="topmenu-left">
+                                <h1> Profil Page </h1> 
+                        </div><!--
+
+                        --><div class="topmenu-right">
+             
+                                <nav>
+                                        <a href="#"><img src="assets/images/notifications.png"></a>
+
+                                        <a href="#"><img src="assets/images/settings.png"></a>
+
+                                        <a href="#"><img src="assets/images/profile.png"></a>
+
+                                </nav>
+                        </div>
+                </div>
+
+                <div class="black-bar">
+
+
+
+                    <div class="left-menu">
+                            <a href="index.html">
+                                    <img src="assets/images/logo.png" alt="My Promus logo">
+                            </a>
+
+                            <nav>
+                                    <ul>
+                                            
+                                            <li>
+                                                   <a href="#"><img src="assets/images/icon-calendar.png" alt="Calendar icon">Calendar</a>
+                                            </li>
+                                            
+                                            <li>
+                                                   <a href="#"><img src="assets/images/icon-events.png" alt="Events icon">Events</a>
+                                            </li>
+
+                                            <li>
+                                                    <a href="#"><img src="assets/images/icon-friends.png" alt="Friends icon">Friends</a>
+                                            </li>
+
+                                    <ul>
+                      
+                            </nav>
+
+                            <div class="menubar-new">
+                                <a href="#"><img src="assets/images/icon-addevent.png">Create Event</a>
+       
+                            </div>
+                    </div>
+
+                </div>
+
+                <div class="grid profil-container">
+                       <h3> Lucas Uzan </h3>
+                        <section class="row">
+
+                                <div id="profil-picture" ><a href="#"><img src="assets/images/profile.png"></a></div>
+                                <ul id="profil-info" >
+                                    <li>Username: Wiiz83</li>
+                                    <li>Email: uzanlah@gmail.com</li>
+                                    <li>City: Cork</li>
+                                    <li>Country: Ireland</li>
+
+                                </ul>
+
+                          
+                        </section>
+                </div>
+
+
+                                </div>
+        </body>
+
+</html>
