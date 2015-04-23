910d8fc1c3c68b685fe4e0ae71194424f08276c5
ferran294 <ferran294@gmail.com>
2015-04-23 19:06:43 +0200

    merge?

--- view/dashboard.php
+++ view/dashboard.php
@@ -4,7 +4,7 @@
         <head>
                 <meta charset="utf-8">
                 <title>My Promus :: Home Page</title>
-                <link rel="stylesheet" href="../view/assets/stylesheets/styles.css">
+                <link rel="stylesheet" href="assets/stylesheets/styles.css">
                 <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700,500,700italic,500italic,400italic,300italic,900,900italic' rel='stylesheet' type='text/css'> 
         </head>
 
@@ -27,40 +27,46 @@
                         </div>
                 </div>
 
-
-                <div class="left-menu">
-                        <a href="http://localhost/myPromus/view/dashboard.php">
-                                <img src="assets/images/logo2.png" alt="My Promus logo">
-                        </a>
-
-                        <nav style="margin-top:40px;">
-                                <ul>
-                                        
-                                       <li style="margin-top:20px">
-                            <a href="http://localhost/myPromus/view/calendar.php"><img src="assets/images/icon-calendar.png" onmouseover="this.src='assets/images/icon-calendar-hover.png'" onmouseout="this.src='assets/images/icon-calendar.png'" alt="Calendar icon">Calendar</a>
-                        </li>
-
-                        <li style="margin-top:20px;"> 
-                            <a href="http://localhost/myPromus/view/events.php"><img src="assets/images/icon-events.png" onmouseover="this.src='assets/images/icon-events-hover.png'" onmouseout="this.src='assets/images/icon-events.png'" alt="Events icon">Events</a>
-                        </li>
-
-                        <li style="margin-top:20px;">
-                            <a href="http://localhost/myPromus/view/friends.php"><img src="assets/images/icon-friends.png" onmouseover="this.src='assets/images/icon-friends-hover.png'" onmouseout="this.src='assets/images/icon-friends.png'" alt="Friends icon">Friends</a>
-                        </li>
+                <div class="black-bar">
 
 
-                                <ul>
-                  
-                        </nav>
 
-                        <div class="menubar-new">
-                            <a href="http://localhost/myPromus/view/create_event.php"><img src="assets/images/icon-addevent.png">Create Event</a>
-   
-                        </div>
+                    <div class="left-menu">
+                            <a href="index.html">
+                                    <img src="assets/images/logo.png" alt="My Promus logo">
+                            </a>
+
+                            <nav>
+                                    <ul>
+                                            
+                                            <li>
+                                                   <a href="#"><img src="assets/images/icon-calendar.png" alt="Calendar icon">Calendar</a>
+                                            </li>
+                                            
+                                            <li>
+                                                   <a href="#"><img src="assets/images/icon-events.png" alt="Events icon">Events</a>
+                                            </li>
+
+                                            <li>
+                                                    <a href="#"><img src="assets/images/icon-friends.png" alt="Friends icon">Friends</a>
+                                            </li>
+
+                                    <ul>
+                      
+                            </nav>
+
+                            <div class="menubar-new">
+                                <a href="#"><img src="assets/images/icon-addevent.png">Create Event</a>
+       
+                            </div>
+                    </div>
+
                 </div>
 
                 <div class="grid">
+                        <h3> Incoming events </h3>
                         <section class="row">
+<<<<<<< HEAD
 
                                 <?php foreach ($events as $event): ?>
                                  
@@ -75,12 +81,137 @@
                                 
                                 -->
                             <?php endforeach ?>
+=======
+                                <section class="event-box col-1-6">
+                                        <img src="assets/images/event-example.png" alt="Event image">
+                                        <p> Name <p>
+                                        <p> Date </p>
+                                        <p> Place </p>
+                                        <p> Description </p>
+                                        <a href="#" class="btn btn-small"> More </a>
+
+                                </section><!--
+                        
+                                --><section class="event-box col-1-6">
+                                        <img src="assets/images/event-example.png" alt="Event image">
+                                        <p> Name <p>
+                                        <p> Date </p>
+                                        <p> Place </p>
+                                        <p> Description </p>
+                                        <a href="#" class="btn btn-small"> More </a>
+
+                                </section><!--
+
+                                --><section class="event-box col-1-6">
+                                        <img src="assets/images/event-example.png" alt="Event image">
+                                        <p> Name <p>
+                                        <p> Date </p>
+                                        <p> Place </p>
+                                        <p> Description </p>
+                                        <a href="#" class="btn btn-small"> More </a>
+
+                                </section><!--
+
+                                --><section class="event-box col-1-6">
+                                        <img src="assets/images/event-example.png" alt="Event image">
+                                        <p> Name <p>
+                                        <p> Date </p>
+                                        <p> Place </p>
+                                        <p> Description </p>
+                                        <a href="#" class="btn btn-small"> More </a>
+
+                                </section><!--
+
+                                --><section class="event-box col-1-6">
+                                        <img src="assets/images/event-example.png" alt="Event image">
+                                        <p> Name <p>
+                                        <p> Date </p>
+                                        <p> Place </p>
+                                        <p> Description </p>
+                                        <a href="#" class="btn btn-small"> More </a>
+
+                                </section><!--
+                                
+                                --><section class="event-box col-1-6">
+                                        <img src="assets/images/event-example.png" alt="Event image">
+                                        <p> Name <p>
+                                        <p> Date </p>
+                                        <p> Place </p>
+                                        <p> Description </p>
+                                        
+                                        <a href="#" class="btn btn-small"> More </a>
+>>>>>>> origin/master
 
                                 
 
 
                         </section>
 
+                        <h3> My events </h3>
+                        <section class="row">
+                                <section class="event-box col-1-6">
+                                        <img src="assets/images/event-example.png" alt="Event image">
+                                        <p> Name <p>
+                                        <p> Date </p>
+                                        <p> Place </p>
+                                        <p> Description </p>
+                                        <a href="#" class="btn btn-small"> More </a>
+
+                                </section><!--
+                        
+                                --><section class="event-box col-1-6">
+                                        <img src="assets/images/event-example.png" alt="Event image">
+                                        <p> Name <p>
+                                        <p> Date </p>
+                                        <p> Place </p>
+                                        <p> Description </p>
+                                        <a href="#" class="btn btn-small"> More </a>
+
+                                </section><!--
+
+                                --><section class="event-box col-1-6">
+                                        <img src="assets/images/event-example.png" alt="Event image">
+                                        <p> Name <p>
+                                        <p> Date </p>
+                                        <p> Place </p>
+                                        <p> Description </p>
+                                        <a href="#" class="btn btn-small"> More </a>
+
+                                </section><!--
+
+                                --><section class="event-box col-1-6">
+                                        <img src="assets/images/event-example.png" alt="Event image">
+                                        <p> Name <p>
+                                        <p> Date </p>
+                                        <p> Place </p>
+                                        <p> Description </p>
+                                        <a href="#" class="btn btn-small"> More </a>
+
+                                </section><!--
+
+                                --><section class="event-box col-1-6">
+                                        <img src="assets/images/event-example.png" alt="Event image">
+                                        <p> Name <p>
+                                        <p> Date </p>
+                                        <p> Place </p>
+                                        <p> Description </p>
+                                        <a href="#" class="btn btn-small"> More </a>
+
+                                </section><!--
+                                
+                                --><section class="event-box col-1-6">
+                                        <img src="assets/images/event-example.png" alt="Event image">
+                                        <p> Name <p>
+                                        <p> Date </p>
+                                        <p> Place </p>
+                                        <p> Description </p>
+                                        
+                                        <a href="#" class="btn btn-small"> More </a>
+
+                                </section>
+
+
+                        </section>
                 </div>
 
         </body>
