cae7dc9cc0d9817c8afcbbbca7b7d3a4bbcdbcf0
Wiiz83 <uzanlah@gmail.com>
2015-04-24 20:05:48 +0100

    Caca

--- view/includes/menu.php
+++ view/includes/menu.php
@@ -1,21 +1,21 @@
 
-        <body>
-                <div class="top-menu">
-                        <div class="topmenu-left">
-                                <h1> Profil Page </h1> 
-                        </div><!--
+<body>
+    <div class="top-menu">
+        <div class="topmenu-left">
+            <h1> Profil Page </h1> 
+                            </div>
 
-                        --><div class="topmenu-right">
-             
-                                <nav>
-                                        <a href="#"><img src="assets/images/notifications.png"></a>
+                            <div class="topmenu-right">
 
-                                        <a href="#"><img src="assets/images/settings.png"></a>
+                        <nav>
+                            <a href="../controller/dashboard.php"><img src="../view/assets/images/notifications.png"></a>
 
-                                        <a href="#"><img src="assets/images/profile.png"></a>
+                            <a href="../controller/dashboard.php"><img src="../view/assets/images/settings.png"></a>
 
-                                </nav>
-                        </div>
+                            <a href="../controller/dashboard.php"><img src="../view/assets/images/profile.png"></a>
+
+                        </nav>
+                    </div>
                 </div>
 
                 <div class="black-bar">
@@ -23,33 +23,33 @@
 
 
                     <div class="left-menu">
-                            <a href="index.html">
-                                    <img src="assets/images/logo.png" alt="My Promus logo">
-                            </a>
+                        <a href="../controller/dashboard.php">
+                            <img src="../view/assets/images/logo.png" alt="My Promus logo">
+                        </a>
 
-                            <nav>
-                                    <ul>
-                                            
-                                            <li>
-                                                   <a href="#"><img src="assets/images/icon-calendar.png" alt="Calendar icon">Calendar</a>
-                                            </li>
-                                            
-                                            <li>
-                                                   <a href="#"><img src="assets/images/icon-events.png" alt="Events icon">Events</a>
-                                            </li>
+                        <nav>
+                            <ul>
 
-                                            <li>
-                                                    <a href="#"><img src="assets/images/icon-friends.png" alt="Friends icon">Friends</a>
-                                            </li>
+                                <li>
+                                 <a href="../controller/dashboard.php"><img src="../view/assets/images/icon-calendar.png" alt="Calendar icon">Calendar</a>
+                             </li>
 
-                                    <ul>
-                      
+                             <li>
+                                 <a href="../controller/events.php"><img src="../view/assets/images/icon-events.png" alt="Events icon">Events</a>
+                             </li>
+
+                             <li>
+                                <a href="../controller/friends.php"><img src="../view/assets/images/icon-friends.png" alt="Friends icon">Friends</a>
+                            </li>
+
+                            <ul>
+
                             </nav>
 
                             <div class="menubar-new">
-                                <a href="#"><img src="assets/images/icon-addevent.png">Create Event</a>
-       
-                            </div>
-                    </div>
+                                <a href="../controller/create-event.php"><img src="../view/assets/images/icon-addevent.png">Create Event</a>
 
-                </div>
+                            </div>
+                        </div>
+
+                    </div>
