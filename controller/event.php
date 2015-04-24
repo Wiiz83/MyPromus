4f7fb7db1e7b54bda35b6a46a515d887b5d1110b
ferran294 <ferran294@gmail.com>
2015-04-24 02:06:34 +0200

    implemented event controller

--- controller/event.php
+++ controller/event.php
@@ -3,8 +3,35 @@
 Controller of the event page,this page just can be loaded with a GET parameter with the id of the event */
 
 include $_SERVER['DOCUMENT_ROOT'].'/myPromus/includes/session_checker.inc.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/myPromus/model/event_model.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/myPromus/includes/getEventHelper.inc.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/myPromus/model/comment_model.php';
 
-//TO DO pensar que hacer con esto
+session_start();
+
+if(isset($_GET['eventId'])){
+	$userId=$_SESSION['userId'];
+	$eventId=$_GET['eventId'];
+
+	$isAdmin=checkAdmin($userId,$eventId);	//true if is admin and false if not
+	$event=getEvent($eventId);
+
+	if($event==null){
+		$output_error="Ups! this event doesn't exists";
+		include $_SERVER['DOCUMENT_ROOT'].'/myPromus/view/error_output.php';
+		exit();
+	}
+
+	$comments=loadComments($eventId);
+
+	$participants=getParticipants($eventId);
+
+	include $_SERVER['DOCUMENT_ROOT'].'/myPromus/view/event.php';
+
+}else{
+	$output_error="You have to specify an event Id";
+	include $_SERVER['DOCUMENT_ROOT'].'/myPromus/view/error_output.php';
+}
 
 
 ?>
