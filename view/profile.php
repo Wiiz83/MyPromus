01cef23dccd87acca81a54921111ff1bf861461b
Wiiz83 <uzanlah@gmail.com>
2015-04-24 17:52:55 +0100

    Profile modifications

--- view/profile.php
+++ view/profile.php
@@ -76,12 +76,48 @@
 
                                 </ul>
 
-                          
+                                                          
+                <a href="">Update My Profil</a>
+                <a href="">Delete My Profil</a>
+                <a href="">Change My Password</a>
                         </section>
                 </div>
 
+  <div class="grid profil-container">
+
+                <table>
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th>
+    <th>Savings</th>
+  </tr>
+  <tr>
+    <td>Peter</td>
+    <td>Griffin</td>
+    <td>$100</td>
+  </tr>
+  <tr>
+    <td>Lois</td>
+    <td>Griffin</td>
+    <td>$150</td>
+  </tr>
+  <tr>
+    <td>Joe</td>
+    <td>Swanson</td>
+    <td>$300</td>
+  </tr>
+  <tr>
+    <td>Cleveland</td>
+    <td>Brown</td>
+    <td>$250</td>
+</tr>
+</table>
+
+</div>
 
                                 </div>
+
+
         </body>
 
 </html>
