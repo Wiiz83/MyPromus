0343f19543970a1a3733733b917d46fb144b290f
DavidCatalan <freedavid94@gmail.com>
2015-05-01 22:40:52 +0200

    Search friends page "finished"

---
+++ view/search-friends.php
@@ -0,0 +1,123 @@
+	
+	<?php include '../view/includes/header.php'; ?>
+	<?php include '../view/includes/menu.php'; ?>
+
+	<div class="grid">
+		<div class="panel">
+
+			<form class="search-friend" method="post">
+				<input type="text" name="search">
+				<input type="submit" class="btn" value="Search">
+			</form>
+
+			<h6>Suggested Friends</h6>
+
+			<div class="row-main">
+			
+				<section class="col-25">
+					<div class="friend-box">
+
+						<img class="profile-icon" src="../view/assets/images/profile.png" alt="Profile picture">
+						<p>Pablo Cabezas</p>
+						<p>24</p>
+						<p>Alicante, Spain</p>
+						<button type="submit" class="btn">Add</button>
+					</div>
+
+				</section><!--
+
+				--><section class="col-25">
+					<div class="friend-box">
+
+						<img class="profile-icon" src="../view/assets/images/profile.png" alt="Profile picture">
+						<p>Pablo Cabezas</p>
+						<p>24</p>
+						<p>Alicante, Spain</p>
+						<button type="submit" class="btn">Add</button>
+					</div>
+
+				</section><!--
+
+				--><section class="col-25">
+					<div class="friend-box">
+
+						<img class="profile-icon" src="../view/assets/images/profile.png" alt="Profile picture">
+						<p>Pablo Cabezas</p>
+						<p>24</p>
+						<p>Alicante, Spain</p>
+						<button type="submit" class="btn">Add</button>
+					</div>
+
+				</section><!--
+
+				--><section class="col-25">
+					<div class="friend-box">
+
+						<img class="profile-icon" src="../view/assets/images/profile.png" alt="Profile picture">
+						<p>Pablo Cabezas</p>
+						<p>24</p>
+						<p>Alicante, Spain</p>
+						<button type="submit" class="btn">Add</button>
+					</div>
+
+				</section>
+
+			</div>
+
+			<div class="row-main">
+			
+				<section class="col-25">
+					<div class="friend-box">
+
+						<img class="profile-icon" src="../view/assets/images/profile.png" alt="Profile picture">
+						<p>Pablo Cabezas</p>
+						<p>24</p>
+						<p>Alicante, Spain</p>
+						<button type="submit" class="btn">Add</button>
+					</div>
+
+				</section><!--
+
+				--><section class="col-25">
+					<div class="friend-box">
+
+						<img class="profile-icon" src="../view/assets/images/profile.png" alt="Profile picture">
+						<p>Pablo Cabezas</p>
+						<p>24</p>
+						<p>Alicante, Spain</p>
+						<button type="submit" class="btn">Add</button>
+					</div>
+
+				</section><!--
+
+				--><section class="col-25">
+					<div class="friend-box">
+
+						<img class="profile-icon" src="../view/assets/images/profile.png" alt="Profile picture">
+						<p>Pablo Cabezas</p>
+						<p>24</p>
+						<p>Alicante, Spain</p>
+						<button type="submit" class="btn">Add</button>
+					</div>
+
+				</section><!--
+
+				--><section class="col-25">
+					<div class="friend-box">
+
+						<img class="profile-icon" src="../view/assets/images/profile.png" alt="Profile picture">
+						<p>Pablo Cabezas</p>
+						<p>24</p>
+						<p>Alicante, Spain</p>
+						<button type="submit" class="btn">Add</button>
+					</div>
+
+				</section>
+
+			</div>					
+
+		</div>
+
+	</div>
+</body>
+</html>
