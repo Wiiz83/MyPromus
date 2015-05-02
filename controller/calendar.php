c9eeea2a655836332dbc09aa808d93f971cba642
DavidCatalan <freedavid94@gmail.com>
2015-05-02 19:45:56 +0200

    Starting with the calendar

---
+++ controller/calendar.php
@@ -0,0 +1,12 @@
+<?php
+
+	include $_SERVER['DOCUMENT_ROOT'].'/myPromus/includes/session_checker.inc.php'; //Check if the session has expired,in that case redirect to the main page
+	require $_SERVER['DOCUMENT_ROOT'].'/myPromus/model/dashboard_model.php';
+
+	$titlePage="Calendar";
+	$userId=$_SESSION['userId'];
+
+	$events = getNearestEvents( $userId, 80 );
+	}
+
+?>
--- view/calendar.php
+++ view/calendar.php
@@ -10,11 +10,12 @@
 
     <link rel="stylesheet" href="../view/assets/stylesheets/styles.css">
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700,500,700italic,500italic,400italic,300italic,900,900italic' rel='stylesheet' type='text/css'> 
-    <link href='../view/assets/stylesheets/fullcalendar.css' rel='stylesheet' />
+    
     <link href='../view/assets/stylesheets/fullcalendar.print.css' rel='stylesheet' media='print' />
-
+    <link href='../view/assets/stylesheets/fullcalendar.css' rel='stylesheet'/>
+    <script src='../view/assets/js/jquery.min.js'></script>
     <script src='../view/assets/js/moment.min.js'></script>
-    <script src='../view/assets/js/fullcalendar.min.js'></script> 
+    <script src='../view/assets/js/fullcalendar.js'></script> 
 
 
 <script>
@@ -33,57 +34,10 @@
             events: [
                 {
                     title: 'All Day Event',
-                    start: '2015-02-01'
-                },
-                {
-                    title: 'Long Event',
-                    start: '2015-02-07',
-                    end: '2015-02-10'
-                },
-                {
-                    id: 999,
-                    title: 'Repeating Event',
-                    start: '2015-02-09T16:00:00'
-                },
-                {
-                    id: 999,
-                    title: 'Repeating Event',
-                    start: '2015-02-16T16:00:00'
-                },
-                {
-                    title: 'Conference',
-                    start: '2015-02-11',
-                    end: '2015-02-13'
-                },
-                {
-                    title: 'Meeting',
-                    start: '2015-02-12T10:30:00',
-                    end: '2015-02-12T12:30:00'
-                },
-                {
-                    title: 'Lunch',
-                    start: '2015-02-12T12:00:00'
-                },
-                {
-                    title: 'Meeting',
-                    start: '2015-02-12T14:30:00'
-                },
-                {
-                    title: 'Happy Hour',
-                    start: '2015-02-12T17:30:00'
-                },
-                {
-                    title: 'Dinner',
-                    start: '2015-02-12T20:00:00'
-                },
-                {
-                    title: 'Birthday Party',
-                    start: '2015-02-13T07:00:00'
-                },
-                {
-                    title: 'Click for Google',
-                    url: 'http://google.com/',
-                    start: '2015-02-28'
+                    start: '2015-02-01',
+                    editable: false,
+                    url: 'http://www.google.com'
+                
                 }
             ]
         });
