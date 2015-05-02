ca7950da6c5da6e6eee9f4b8109397b85f2ac9bd
ferran294 <ferran294@gmail.com>
2015-05-02 19:13:13 +0200

    calendar server implemented

--- model/calendar.php
+++ model/calendar.php
@@ -4,6 +4,26 @@ Model functions to pass data from the database to the calendar controller*/
 require $_SERVER['DOCUMENT_ROOT'].'/myPromus/includes/db_connection.inc.php';
 
 
+//Return all the events in an associative array ready to get encode in JSON to be used in the Calendar
+function getEvents($userId){
+
+	global $link;
+
+	$sql="SELECT  event.*
+	 FROM event_friend AS ef INNER JOIN event ON ef.event_id=event.id
+	 WHERE ef.friend_id='$userId';";
+
+	 $result=mysqli_query($link,$sql) or die(mysqli_error($link));
+
+	 while ($eventInfo=mysqli_fetch_assoc($result)) {
+	 	$event=array("title"=>$eventInfo['name'],"start"=>$eventInfo['date'],"end"=>$eventInfo['date'],"url"=>"../controller/event.php?eventId=".$eventInfo['id'],"editable"=>"false");
+	 	$events[]=$event;                  
+	 }
+
+
+
+	 return $events; //return the events encoded in a JSON format
+}
 
 
 ?>
