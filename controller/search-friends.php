fd087b2cf87028106b19e7c3ec73b2c7d383ef63
ferran294 <ferran294@gmail.com>
2015-05-08 19:56:20 +0200

    shuffle deleted

--- controller/search-friends.php
+++ controller/search-friends.php
@@ -19,7 +19,7 @@ if(isset($_POST['search'])){
 }else{
 
 	$users=getFriendSuggestions($userId,20); //it's an array of user,can be null
-	shuffle($users); //Sort randomly the array
+	
 	$titleSecondary="Suggested friends";
 	include $_SERVER['DOCUMENT_ROOT'].'/myPromus/view/search-friends.php';
 
