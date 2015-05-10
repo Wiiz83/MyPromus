4506e59d9a6bc977cec213f44cae8c5666639089
audric_manaud <audric.manaud@gmail.com>
2015-05-10 20:15:58 +0100

    Add : addSong, functio that adds a song to a playlist

--- model/event_model.php
+++ model/event_model.php
@@ -221,5 +221,27 @@ function searchSong($song){
 	return json_encode($results);
 }
 
+function addSong($userId, $playlistId, $songId)
+{
+	$session = new SpotifyWebAPI\Session('730c01f53af44936a0cc51459f0cb0ea', 'e1fc633ca35141bdb6edca04632850e7', '');
+	$api = new SpotifyWebAPI\SpotifyWebAPI();
+
+	$refreshToken = $_SESSION['refreshToken'];
+
+	$session->setRefreshToken($refreshToken);
+	$session->refreshAccessToken();
+
+	$accessToken = $session->getAccessToken();
+
+	// Request a access token using the code from Spotify
+	$refreshToken = $session->getRefreshToken();
+	$_SESSION['refreshToken'] = $refreshToken;
+
+	// Set the new access token on the API wrapper
+	$api->setAccessToken($accessToken);
+
+	$api->addUserPlaylistTracks($usernameId, $playlistId, array($songId));
+}
+
 
 ?>
