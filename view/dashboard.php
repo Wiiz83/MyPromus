e9472342faf25945f43aac62395dfd8f9490a148
Wiiz83 <uzanlah@gmail.com>
2015-05-11 13:42:41 +0100

    dash final

--- view/dashboard.php
+++ view/dashboard.php
@@ -129,96 +129,53 @@
         <p class="text">Cork</p>
         </div>
          
+  </div>
+    <div class="item tendance">
 
+        <img src="../view/assets/images/event-example.png" alt="">
+        <div class="textbox">
+        <a href=""><p class="text">Ultra Korea</p></a>
+        <a href=""><p class="text">By Audric Manaud</p></a>
+        <p class="text">17/07/2015</p>
+        <p class="text">Cork</p>
+        </div>
+         
+  </div>
+    <div class="item tendance">
 
+        <img src="../view/assets/images/event-example.png" alt="">
+        <div class="textbox">
+        <a href=""><p class="text">Ultra Korea</p></a>
+        <a href=""><p class="text">By Audric Manaud</p></a>
+        <p class="text">17/07/2015</p>
+        <p class="text">Cork</p>
+        </div>
+         
+  </div>
+    <div class="item tendance">
+
+        <img src="../view/assets/images/event-example.png" alt="">
+        <div class="textbox">
+        <a href=""><p class="text">Ultra Korea</p></a>
+        <a href=""><p class="text">By Audric Manaud</p></a>
+        <p class="text">17/07/2015</p>
+        <p class="text">Cork</p>
+        </div>
+         
+  </div>
+    <div class="item tendance">
+
+        <img src="../view/assets/images/event-example.png" alt="">
+        <div class="textbox">
+        <a href=""><p class="text">Ultra Korea</p></a>
+        <a href=""><p class="text">By Audric Manaud</p></a>
+        <p class="text">17/07/2015</p>
+        <p class="text">Cork</p>
+        </div>
+         
+  </div>
+         
   </div>
 </div>
 </body>
 </html>
-<!-- 
-<script type="text/javascript">
-$(document).ready(function() {
-    $('.pgwSlideshow').pgwSlideshow();
-});
-</script>
-<div class="grid">
-    <section class="row">
-    <div id="event-bloc">
-        <h6>Your Events Summary</h6>
-        <ul id="event-info">
-            <li id="title">
-               <?php if(isset($events)): ?>
-                        <?php foreach($events as $event): ?>
-                <a href="../controller/event.php?eventId=<?php echo $event->getIdEvent(); ?>">
-                <p><?php echo $event->getName(); ?></p>
-                </a>
-                </li>
-            <li id="playlist">
-                <iframe src="https://embed.spotify.com/?uri=spotify%3Auser%3Aqlmhuge%3Aplaylist%3A6Df19VKaShrdWrAnHinwVO&theme=white" width="300" height="380" frameborder="0" allowtransparency="true">
-            </iframe>
-            </li>
-        </ul>
-        <ul class="pgwSlideshow" >
-                <li><img src="<?php echo $event->getImage(); ?>" alt="<?php echo $event->getPlace(); ?>"  data-description="<?php echo $event->getDate(),' at ',$event->getTime(); ?>"></li>
-                <?php endforeach ?>
-                <?php else: ?>
-                       <a href="../controller/create-event.php">
-                      <p>LET'S CREATE MY FIRST EVENT</p>
-                     </a>
-                    </li>
-                </ul>
-                <ul class="pgwSlideshow" >
-                <li><img src="../view/assets/images/event-example.png"></li>
-                <div class="ps-list" style="display:none !important;">
-                <?php endif ?>    
-        </ul>
-    </div>
-    <div id="friend-bloc">
-        <h6>Your Friends Activity</h6>
-        <div id="friends-column">
-            <ul>
-                <li><a href="../controller/profile.php"><img src="../view/assets/images/event-example.png"/></a>
-                <p>
-                    Lucas Uzan go to Yolo Party
-                </p>
-                </li>
-                <li><a href="../controller/profile.php"><img src="../view/assets/images/event-example.png"/></a>
-                <p>
-                    Lucas Uzan go to Yolo Party
-                </p>
-                </li>
-                <li><a href="../controller/profile.php"><img src="../view/assets/images/event-example.png"/></a>
-                <p>
-                    Lucas Uzan go to Yolo Party
-                </p>
-                </li>
-                </li>
-                <li><a href="../controller/profile.php"><img src="../view/assets/images/event-example.png"/></a>
-                <p>
-                    Lucas Uzan go to Yolo Party
-                </p>
-                </li>
-            </ul>
-        </div>
-    </div>
-    <div id="tendance-bloc">
-        <h6>The Popular Events</h6>
-            <div id="incoming">
-                                         <div class="item tendance">
-                                        <a href=""><img class="event-pic" src="../view/assets/images/event-example.png" alt="Event picture"></a>
-                                        <br/> <br/>
-                                         <span class="event-name"> YOLO PARTY </span>
-                                                 <span><i>24 People Coming<i></span>
-                                                <p><b>Owner: </b>LUCAS</p>
-                                                <p><b>Date:</b> 17/07/1949</p>
-                                                <p><b>Place:</b> Cork </p>
-                                      </div>
-                                    </div>
-                                    <div class="customNavigation">
-                                                <span class="prev prev-inco"></span>
-                                                 <span class="next next-inco"></span>
-                                    </div>
-    </div>
-</div>
-</body>
-</html> -->
